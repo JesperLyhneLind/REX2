578ac32bfac1c10aa0d47e62efdd7d6a4e8bd700
cfp185 <cfp185@alumni.ku.dk>
2023-09-29 14:32:07 +0200

    Update detection.py

--- REX/detection.py
+++ REX/detection.py
@@ -71,18 +71,18 @@ def go_to_box(angle_sign, angle, dist, ids):
         print("going to box")
         print("id: ", ids)
         print("dist: ", dist) # dist 678 // Hvorfor er dist 678? / Hvor kommer 678 fra?
-        print("actual dist:", (dist - 500) / 100) # in meters.
+        print("actual dist:", (dist - 500) / 1000) # in meters.
         if angle_sign == -1:
             #turn(Direction.Left, angle)
             #if dist < 800
-            iDrive((dist - 500) / 100)
+            iDrive((dist - 500) / 1000)
             #iDrive(1)
         elif angle_sign == 1:
             #turn(Direction.Right, angle)
-            iDrive((dist - 500) / 100)
+            iDrive((dist - 500) / 1000)
             #iDrive(1)
         else:
-            iDrive((dist - 500) / 100)
+            iDrive((dist - 500) / 1000)
             #iDrive(1)
 
 params = aruco.DetectorParameters_create()
