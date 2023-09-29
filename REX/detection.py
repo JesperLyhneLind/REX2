8257536be7518dc78bfc65370dc12643537eb2e7
cfp185 <cfp185@alumni.ku.dk>
2023-09-29 14:06:18 +0200

    Lad os ikke lave en meter igen

--- REX/detection.py
+++ REX/detection.py
@@ -62,7 +62,7 @@ def turn(dir: Direction, angle: int):
 def iDrive(meters):
     #print(arlo.go_diff(70, 70, 1, 1))
     print(arlo.go_diff(70, 70, 1, 1))
-    sleep(1.58*meters)
+    sleep(1.75*meters)
     print(arlo.stop())
     sleep(0.18)
     # Wait a bit while robot moves forward
