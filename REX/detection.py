ae01ebba3b77d10df22263f0b506609bcb5c4cde
cfp185 <cfp185@alumni.ku.dk>
2023-09-29 13:28:35 +0200

    f

--- REX/detection.py
+++ REX/detection.py
@@ -51,12 +51,12 @@ def turn(dir: Direction, angle: int):
         print(arlo.go_diff(40, 40, 0, 1))
         sleep(angle/65)
         print(arlo.stop())
-        sleep(0.15)
+        sleep(0.18)
     else:
         print(arlo.go_diff(40, 40, 1, 0))
         sleep(angle/65)
         print(arlo.stop())
-        sleep(0.15)
+        sleep(0.18)
 
 # Drives one meter.
 def driveM(meters):
