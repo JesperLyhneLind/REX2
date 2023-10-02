c3eb33be169d88dc94004fe128cb2d6b2ea43392
cfp185 <cfp185@alumni.ku.dk>
2023-10-02 13:54:33 +0200

    Update detection.py

--- REX/detection.py
+++ REX/detection.py
@@ -157,6 +157,6 @@ while cv2.waitKey(4) == -1:  # Wait for a key pressed event
         print("vec:", vec)
         print("angle:", angle)
         print("ids", ids)
-        # go_to_box(angle_sign[0], angle, dist, ids[maxvecidx])
-    else:
-        turn(Direction.Right, 45)
+        go_to_box(angle_sign[0], angle, dist, ids[maxvecidx])
+    #else:
+        #turn(Direction.Right, 45)
