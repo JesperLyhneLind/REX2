5ee33f10b077aa3863dbc517eb7e56469ba98324
JesperLyhneLind <jesperlyhnelind@icloud.com>
2023-10-09 14:37:40 +0200

    Update grids.py

--- REX/grids.py
+++ REX/grids.py
@@ -35,14 +35,15 @@ class GridOccupancyMap(object):
         """
         generate a grid map with some circle shaped obstacles
         """
-        radius = np.random.uniform(low=1.4, high=1.4, size=n_obs)
+        radius= np.empty((1, n_obs))
+        radius.fill(3.7)
         #fill the grids by checking if the grid centroid is in any of the circle
         for i in range(self.n_grids[0]):
             for j in range(self.n_grids[1]):
-                point = np.array([self.map_area[0][0] + self.resolution * i, 
-                                  self.map_area[0][1] + self.resolution * j])
+                centroid = np.array([self.map_area[0][0] + self.resolution * (i+0.5), 
+                                     self.map_area[0][1] + self.resolution * (j+0.5)])
                 for o, r in zip(coords, radius):
-                    if o[0]-r <= point[0] < o[0]+r and o[1]-r <= point[1] < o[1]+r:
+                    if np.linalg.norm(centroid - o) <= r:
                         self.grid[i, j] = 1
                         break
         
