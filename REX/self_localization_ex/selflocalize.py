83b82cabff5df0e091a28a0b876c3e52e1ad9f27
JesperLyhneLind <jesperlyhnelind@icloud.com>
2023-11-01 10:35:25 +0100

    Update selflocalize.py

--- REX/self_localization_ex/selflocalize.py
+++ REX/self_localization_ex/selflocalize.py
@@ -253,7 +253,7 @@ try:
             print(np.std(normalized_weights))
 
             #sat op fra 0.00015
-            if np.std(normalized_weights) < 0.0013:
+            if np.std(normalized_weights) < 0.0011:
                 break
         
         else:
