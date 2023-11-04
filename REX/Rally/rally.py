304e6db66aca1fce2e0efea268725d4a4bd4b2e2
JesperLyhneLind <jesperlyhnelind@icloud.com>
2023-11-04 13:47:12 +0100

    Update rally.py

--- REX/Rally/rally.py
+++ REX/Rally/rally.py
@@ -16,10 +16,10 @@ otto = robot.Robot()
 
 # Landmarks.
 # The robot knows the position of 4 landmarks. Their coordinates are in the unit centimeters [cm].
-landmarkIDs = [1, 2, 3, 4]
+landmarkIDs = [6, 7, 3, 4]
 landmarks = {
-    1: (0.0, 0.0),  # Coordinates for landmark 1
-    2: (0.0, 300.0),  # Coordinates for landmark 2
+    6: (0.0, 0.0),  # Coordinates for landmark 1
+    7: (0.0, 300.0),  # Coordinates for landmark 2
     3: (400.0, 0.0),  # Coordinates for landmark 3
     4: (400.0, 300.0)  # Coordinates for landmark 4
 }
@@ -29,7 +29,7 @@ num_particles = 1000
 particles = selflocalize_method.initialize_particles(num_particles)
 # The estimate of the robots current pose
 
-est_pose = particle.estimate_pose(particles)
+# est_pose = particle.estimate_pose(particles)
 
 # The estimate of the robots current pose
 robot_pose = particle.estimate_pose(particles) # (x, y, theta)
@@ -57,14 +57,6 @@ ret_particles = selflocalize_method.self_localize(landmarks, landmarkIDs, num_pa
 est_pose = particle.estimate_pose(ret_particles)
 print("est_pose:", est_pose.getX(), est_pose.getY())
 
-# time1 = time.time()
-# while True:
-#     est_pose = particle.estimate_pose(ret_particles)
-#     print("est_pose:", est_pose.getX(), est_pose.getY())
-#     detection.turn(Direction.Right, 30)
-#     time2 = time.time()
-#     if time1 - time2 > 40:
-#         break
 
 # otto.go_diff(70,71,1,1)
 # sleep(2.6*meters)
