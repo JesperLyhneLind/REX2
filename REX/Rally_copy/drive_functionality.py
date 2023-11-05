de5c732caffac0f9089e6b94454426bb0d058314
KatjaBorchorst <wkl712@alumni.ku.dk>
2023-11-05 23:13:35 +0100

    asd

---
+++ REX/Rally_copy/drive_functionality.py
@@ -0,0 +1,86 @@
+import time
+from time import sleep
+import robot
+from enum import Enum
+import selflocalize_method
+
+arlo = robot.Robot()
+
+class Direction(Enum):
+    Left = 1
+    Right = 2
+
+# Checks if there's any object in the path of the robot.  
+def check():
+    #while(True):
+    Front_sensor = arlo.read_front_ping_sensor()
+    Right_sensor = arlo.read_right_ping_sensor()
+    Left_sensor = arlo.read_left_ping_sensor()
+
+        # if Left_sensor < 300 or Right_sensor < 300 or Front_sensor < 400:
+        #     print("Left: " + str(Left_sensor))
+        #     print("Front: " + str(Front_sensor))
+        #     print("Right: " + str(Right_sensor))
+    return Left_sensor, Right_sensor, Front_sensor
+        
+# Turns the robot angle degrees.
+def turn(dir: Direction, angle: int):
+    if dir == Direction.Left:
+        print(arlo.go_diff(40, 40, 0, 1))
+        sleep(angle/65) 
+        print(arlo.stop())
+        sleep(0.18)
+    else:
+        print(arlo.go_diff(40, 40, 1, 0))
+        sleep(angle/65)
+        print(arlo.stop())
+        sleep(0.18)
+
+# Drives a certain amount of meters until obstacle is detected in front of it.
+def iDrive(meters):
+    print(arlo.go_diff(70, 71, 1, 1))
+    print("driving meters ", meters)
+    start = time.perf_counter()
+    while True:
+        Left_sensor, Right_sensor, Front_sensor = check()
+        if Front_sensor < 400 or Left_sensor < 400 or Right_sensor < 400:
+            print(arlo.stop())
+            print("Left: " + str(Left_sensor))
+            print("Front: " + str(Front_sensor))
+            print("Right: " + str(Right_sensor))
+            print("oh no, obstacle detected!! :( miv)")
+            return 1 # Otto stopped because of obstacle
+        if (time.perf_counter() - start > (2.6*meters)):
+            print(arlo.stop())
+            sleep(0.18)
+            print("oh yes I drived meters ", meters)
+            return 0 # Otto drived intended distance
+   
+
+
+
+# Drives the robot and checks which direction to go for avoiding an object.
+def drive(): 
+    #arlo.go_diff(70, 71, 1, 1)
+    Left_sensor, Right_sensor, Front_sensor = check()
+
+    if Left_sensor >= Right_sensor:
+        print("left")
+        turn(Direction.Left, 45)
+        iDrive(1)
+        turn(Direction.Right, 90)
+        iDrive(1)
+        turn(Direction.Left, 45)
+        iDrive(1)
+        
+
+    elif Right_sensor > Left_sensor:
+        print("Right")
+        turn(Direction.Right, 45)
+        iDrive(1)
+        turn(Direction.Left, 90)
+        iDrive(1)
+        turn(Direction.Right, 45)
+        iDrive(1)
+    else:
+        pass
---
+++ REX/Rally_copy/drivingStrategy.py
@@ -0,0 +1,95 @@
+import cv2
+import sys
+import numpy as np
+import robot
+import math
+import particle
+import selflocalize_method
+from time import sleep
+from enum import Enum
+import drive_functionality
+
+
+class Direction(Enum):
+    Left = 1
+    Right = 2
+
+
+# Landmarks.
+# The robot knows the position of 4 landmarks. Their coordinates are in the unit centimeters [cm].
+landmarkIDs = [1, 2, 3, 4]
+landmarks = {
+    1: (0.0, 0.0),  # Coordinates for landmark 1
+    2: (0.0, 300.0),  # Coordinates for landmark 2
+    3: (400.0, 0.0),  # Coordinates for landmark 3
+    4: (400.0, 300.0)  # Coordinates for landmark 4
+}
+#landmarks_inOrder = [1,2,3,4,1]
+
+# Funtion for finding the orientation from the robot towards its next goal in degrees.
+def orientation(id_index, cam):
+    # The estimate of the robots current pose
+    robot_pose = selflocalize_method.self_localize(landmarks, landmarkIDs, cam)
+
+    # Calculate the vector, that the robot should drive to in order to visit the goal.
+    print("goal: ", (landmarks[id_index]))
+    vec_posX = (landmarks[id_index])[0] - robot_pose.getX() # x-coordinate
+    vec_posY = (landmarks[id_index])[1] - robot_pose.getY() # y-coordinate
+
+    # Calculate the new theta.
+    vec_theta = math.degrees(math.atan2(vec_posY, vec_posX) - robot_pose.getTheta())
+ 
+    # Returns the orientation-vector.
+    return vec_theta, vec_posX, vec_posY # degrees instead of radians.
+
+# Avoids an object and drives the robot 0.3m if there's nothing detected in front of it.
+def avoid():
+    Left_sensor, Right_sensor, Front_sensor = drive_functionality.check()
+
+    if Left_sensor >= Right_sensor:
+        print("Turning left - avoid, 45")
+        drive_functionality.turn(Direction.Left, 45)
+    else:
+        print("Turning right - avoid, 45")
+        drive_functionality.turn(Direction.Right, 45)
+
+    drive_functionality.iDrive(0.5)
+
+def canYouSeeTarget(goalID, cam):
+    # Check if it can see target
+    colour = cam.get_next_frame()
+    d_objectIDs, dists, angles = cam.detect_aruco_objects(colour)
+    if not isinstance(d_objectIDs, type(None)):
+        print("goalID in d_objectIDs")
+        return goalID in d_objectIDs
+
+
+# Turns the robot and drives towards the goal while avoiding objects.
+def driveAlongVec(vecX, vecY, theta, goalID, cam):
+    distance = math.sqrt(vecX**2 + vecY**2) # pythagorean theorem.
+    print("driving " + str(distance-40) + " cm to goal along " + str(vecX) + " " + str(vecY))
+    # Let the robot face the goal.
+    if np.sign(theta) == 1:
+        drive_functionality.turn(Direction.Right, theta) # right.
+        print("turning right with degrees ", theta)
+    elif np.sign(theta) == -1:
+        drive_functionality.turn(Direction.Left, abs(theta)) # left.
+        print("turning right with degrees ", abs(theta))
+    # If yes go until sensor sensors
+        # if canYouSeeTarget(goalID, cam):
+        #     print("I can see the target!!")
+        #     res = drive_functionality.iDrive(40) # Drives until reaching landmark.
+        #     return 2
+    # If not do as before:
+    # Drives the robot towards the goal, while there's longer than 0,4m to the goal.
+    if drive_functionality.iDrive((distance-40)/100) == 1:
+        print("avoiding")
+        avoid()
+        return 0 # Ends with avoid
+    else:
+        print("I drived distance:)")
+        return 1 # Intended drived distance.
+
+def OP():
+    return "OWER-POWERED DRIVING STRATEGY BRO"
+        
