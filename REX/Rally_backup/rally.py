9e9d3803ef82d2163d40a3e890efc3b04ebc8419
KatjaBorchorst <wkl712@alumni.ku.dk>
2023-11-05 23:45:12 +0100

    rga

--- REX/Rally_backup/rally.py
+++ REX/Rally_backup/rally.py
@@ -1,33 +1,46 @@
-import cv2
-import sys
-import numpy as np
-import robot
-import math
-import particle
-import drive_functionality
-from time import sleep
-from enum import Enum
-import drivingStrategy
+# import cv2
+# import sys
+# import numpy as np
+# import robot
+# import math
+# import particle
+# import drive_functionality
+# from time import sleep
+# from enum import Enum
+# import drivingStrategy
 
-otto = robot.Robot()
-class Direction(Enum):
-    Left = 1
-    Right = 2
+# otto = robot.Robot()
+# class Direction(Enum):
+#     Left = 1
+#     Right = 2
 
-# Landmarks.
-# The robot knows the position of 4 landmarks. Their coordinates are in the unit centimeters [cm].
-landmarkIDs = [1, 2, 3, 4]
+# # Landmarks.
+# # The robot knows the position of 4 landmarks. Their coordinates are in the unit centimeters [cm].
+# landmarkIDs = [1, 2, 3, 4]
 
-landmarks_inOrder = [1,2,3,4,1]
-landmarks_index = 0
+# landmarks_inOrder = [1,2,3,4,1]
+# landmarks_index = 0
+
+# # while landmarks_index < 5:
+# #     if drivingStrategy.detectLandmark(landmarks_inOrder[landmarks_index]) == 1: # Spotted landmark
+# #         print("Spotted landmark - driving to")
+# #         if drive_functionality.iDrive(1) == 0: # Kachowwwwwwwwww skrrrtt swag lol yolo brrr brrr 888 kesi
+# #             print("Target not reached - i drive more")
+# #             drive_functionality.iDrive(1)
+# #         print("Target reached - next landmark")
+# #         landmarks_index += 1
+# #     else: 
+# #         print("Finding obstacles")
+# #         if drivingStrategy.detectObstacle == 1: # Spotted obstacle
+# #             print("Driving to(wards) obstacle")
+# #             if drive_functionality.iDrive(0.5) == 1: # Stopped due to obstacle
+# #                print("Obstacle detected - avoiding")
+# #                drivingStrategy.avoid() 
 
 # while landmarks_index < 5:
 #     if drivingStrategy.detectLandmark(landmarks_inOrder[landmarks_index]) == 1: # Spotted landmark
 #         print("Spotted landmark - driving to")
-#         if drive_functionality.iDrive(1) == 0: # Kachowwwwwwwwww skrrrtt swag lol yolo brrr brrr 888 kesi
-#             print("Target not reached - i drive more")
-#             drive_functionality.iDrive(1)
-#         print("Target reached - next landmark")
+#         drive_functionality.simpDrive()    
 #         landmarks_index += 1
 #     else: 
 #         print("Finding obstacles")
@@ -36,19 +49,6 @@ landmarks_index = 0
 #             if drive_functionality.iDrive(0.5) == 1: # Stopped due to obstacle
 #                print("Obstacle detected - avoiding")
 #                drivingStrategy.avoid() 
-
-while landmarks_index < 5:
-    if drivingStrategy.detectLandmark(landmarks_inOrder[landmarks_index]) == 1: # Spotted landmark
-        print("Spotted landmark - driving to")
-        drive_functionality.simpDrive()    
-        landmarks_index += 1
-    else: 
-        print("Finding obstacles")
-        if drivingStrategy.detectObstacle == 1: # Spotted obstacle
-            print("Driving to(wards) obstacle")
-            if drive_functionality.iDrive(0.5) == 1: # Stopped due to obstacle
-               print("Obstacle detected - avoiding")
-               drivingStrategy.avoid() 
                 
 
         
