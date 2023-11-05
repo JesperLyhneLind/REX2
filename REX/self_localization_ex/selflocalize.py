6fa6a56ea5cb3d71fb12c9d4d6d5083c086a1b49
JesperLyhneLind <jesperlyhnelind@icloud.com>
2023-11-05 16:32:25 +0100

    Update selflocalize.py

--- REX/self_localization_ex/selflocalize.py
+++ REX/self_localization_ex/selflocalize.py
@@ -154,7 +154,8 @@ try:
 
 
         #particle.add_uncertainty(particles, 4, 0.025) #noise sigmas are centimeter and radians
-        particle.add_uncertainty(particles, 14, 0.25) #noise sigmas are centimeter and radians
+        #particle.add_uncertainty(particles, 14, 0.25) #noise sigmas are centimeter and radians
+        particle.add_uncertainty(particles, 10, 0.25) #noise sigmas are centimeter and radians
         # Fetch next frame
         
         colour = cam.get_next_frame()
@@ -188,7 +189,8 @@ try:
                 for i in range(len(objectIDs)):
                     if objectIDs[i] in landmarkIDs:
                         particle_distance = np.sqrt(((landmarks[objectIDs[i]])[0] - par.getX())**2 + ((landmarks[objectIDs[i]])[1] - par.getY())**2)
-                        sigma_d = 14 # try value 20cm
+                        #sigma_d = 14 # try value 20cm
+                        sigma_d = 10 # try value 20cm
                         p_d = distance_observation_model(dists[i], particle_distance, sigma_d)
                        
                         #angle
