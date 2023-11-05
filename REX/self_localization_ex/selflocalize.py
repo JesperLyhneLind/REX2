729d247ac471421090fb7d3da2bbe9d036742509
JesperLyhneLind <jesperlyhnelind@icloud.com>
2023-11-05 14:09:14 +0100

    Update selflocalize.py

--- REX/self_localization_ex/selflocalize.py
+++ REX/self_localization_ex/selflocalize.py
@@ -40,7 +40,7 @@ CWHITE = (255, 255, 255)
 CBLACK = (0, 0, 0)
 # Landmarks.
 # The robot knows the position of 2 landmarks. Their coordinates are in the unit centimeters [cm].
-landmarkIDs = [6, 7]
+landmarkIDs = [1, 2, 3, 4]
 landmarks = {
     1: (0.0, 0.0),  # Coordinates for landmark 1
     2: (0.0, 300.0),  # Coordinates for landmark 2
