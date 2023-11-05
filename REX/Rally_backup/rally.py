26cf227fb87a502b8ee059e5c539cc6d9e5d4287
KatjaBorchorst <wkl712@alumni.ku.dk>
2023-11-05 23:29:45 +0100

    Update rally.py

--- REX/Rally_backup/rally.py
+++ REX/Rally_backup/rally.py
@@ -21,12 +21,26 @@ landmarkIDs = [1, 2, 3, 4]
 landmarks_inOrder = [1,2,3,4,1]
 landmarks_index = 0
 
+# while landmarks_index < 5:
+#     if drivingStrategy.detectLandmark(landmarks_inOrder[landmarks_index]) == 1: # Spotted landmark
+#         print("Spotted landmark - driving to")
+#         if drive_functionality.iDrive(1) == 0: # Kachowwwwwwwwww skrrrtt swag lol yolo brrr brrr 888 kesi
+#             print("Target not reached - i drive more")
+#             drive_functionality.iDrive(1)
+#         print("Target reached - next landmark")
+#         landmarks_index += 1
+#     else: 
+#         print("Finding obstacles")
+#         if drivingStrategy.detectObstacle == 1: # Spotted obstacle
+#             print("Driving to(wards) obstacle")
+#             if drive_functionality.iDrive(0.5) == 1: # Stopped due to obstacle
+#                print("Obstacle detected - avoiding")
+#                drivingStrategy.avoid() 
+
 while landmarks_index < 5:
     if drivingStrategy.detectLandmark(landmarks_inOrder[landmarks_index]) == 1: # Spotted landmark
         print("Spotted landmark - driving to")
-        while drive_functionality.iDrive(1) == 0: # Kachowwwwwwwwww skrrrtt swag lol yolo brrr brrr 888 kesi
-            print("Target not reached - i drive more")
-        print("Target reached - next landmark")
+        drive_functionality.simpDrive()    
         landmarks_index += 1
     else: 
         print("Finding obstacles")
